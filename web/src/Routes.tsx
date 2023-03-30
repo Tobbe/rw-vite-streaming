6f4df3b255b7e917a2d0bc0870f147cbc6501cff
Tobbe Lundberg <tobbe@tlundberg.com>
2023-03-30 19:10:28 +0700

    Some test pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,12 +7,26 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route } from '@redwoodjs/router'
+
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
+
+import MainLayout from './layouts/MainLayout/MainLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route notfound page={NotFoundPage} />
+      <Set wrap={MainLayout}>
+        <Set wrap={ScaffoldLayout} title="UserExamples" titleTo="userExamples" buttonLabel="New UserExample" buttonTo="newUserExample">
+          <Route path="/user-examples/new" page={UserExampleNewUserExamplePage} name="newUserExample" />
+          <Route path="/user-examples/{id:Int}/edit" page={UserExampleEditUserExamplePage} name="editUserExample" />
+          <Route path="/user-examples/{id:Int}" page={UserExampleUserExamplePage} name="userExample" />
+          <Route path="/user-examples" page={UserExampleUserExamplesPage} name="userExamples" />
+        </Set>
+        <Route path="/about" page={AboutPage} name="about" prerender />
+        <Route path="/" page={HomePage} name="home" prerender />
+        <Route notfound page={NotFoundPage} />
+      </Set>
     </Router>
   )
 }

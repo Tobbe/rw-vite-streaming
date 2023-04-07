e16ad23767f31bdc1721eb9e714c4c06ed7eaf47
Tobbe Lundberg <tobbe@tlundberg.com>
2023-04-07 12:51:59 +0200

    Update baremetal config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'serve',
+      name: 'api',
       cwd: 'current',
       script: 'node_modules/.bin/rw',
-      args: 'serve',
+      args: 'serve api',
+      instances: 'max',
+      exec_mode: 'cluster',
+      wait_ready: true,
+      listen_timeout: 10000,
+    },
+    {
+      name: 'fe',
+      cwd: 'current/web',
+      script: 'yarn',
+      args: 'rw-serve-fe',
       instances: 'max',
       exec_mode: 'cluster',
       wait_ready: true,
